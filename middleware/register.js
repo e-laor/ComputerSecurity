1f96dec006202dbc72238256857f092d876a318c
osher <oshrico2@gmail.com>
2024-06-17 14:40:05 +0300

    update bcrypt libary

--- middleware/register.js
+++ middleware/register.js
@@ -1,7 +1,8 @@
-const bcrypt = require('bcrypt');
-const User = require('../model/User');
+const bcrypt = require("bcrypt");
+const { Op } = require("sequelize");
+const User = require("../model/User");
 const pass_secure = require("../security/pass_security");
-const PasswordHistory = require('../model/PasswordHistory');
+const PasswordHistory = require("../model/PasswordHistory");
 
 async function register(req, res, next) {
   try {
@@ -13,6 +14,7 @@ async function register(req, res, next) {
     if (!password || !confirm_password) {
       throw new Error("Passwords can't be empty");
     }
+
     // Check if the username already exists
     const existingUserByUsername = await User.findOne({
       where: { username: username },
@@ -22,30 +24,38 @@ async function register(req, res, next) {
     }
 
     // Check if the email already exists
-    const existingUserByEmail = await User.findOne({ where: { email: email } });
+    const existingUserByEmail = await User.findOne({
+      where: { email: email },
+    });
     if (existingUserByEmail) {
       throw new Error("Email already registered");
     }
 
-    // check password strength
+    // Check password strength
     const isValidPassword = pass_secure.isPasswordStrong(password, username);
 
-    // if password isn't strong enough throw an error with the message
+    // If password isn't strong enough, throw an error with the message
     if (!isValidPassword.isValid) {
       throw new Error(isValidPassword.message);
     }
 
+    // Hash the password
     const saltRounds = 10;
     const hashedPassword = await bcrypt.hash(password, saltRounds);
+
+    // Create user using Sequelize's create method with parameters
     const user = await User.create({
       username,
       email,
       password: hashedPassword,
     });
+
+    // Store password history (assuming PasswordHistory model is used)
     await PasswordHistory.create({
       userId: user.id,
-      password: hashedPassword
-    })
+      password: hashedPassword,
+    });
+
     req.flash("success", "You have registered successfully.");
     res.redirect("/register");
   } catch (error) {
